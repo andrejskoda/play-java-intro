951c7a288814ea48afbfe8c07b507dcc8e2bbbdb
andrej <andrejskoda@gmail.com>
2015-07-07 12:38:43 +0200

    parsing and persisting offices from json file

--- app/Global.java
+++ app/Global.java
@@ -1,19 +1,35 @@
 /**
  * Created by andrej on 07.07.15.
  */
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import models.Office;
-import play.*;
+import play.Application;
+import play.GlobalSettings;
+import play.Logger;
 import play.db.jpa.JPA;
-import play.db.jpa.Transactional;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Global extends GlobalSettings {
 
     @Override
     public void onStart(Application app) {
+        Office[] offices= readJson();;
         JPA.withTransaction(()->{
-            Office office = new Office();
-            office.name="testName";
-            JPA.em().persist(office);
+//            Office office = new Office();
+//            office.id="testId";
+//            office.name="testName";
+
+            for (Office office : offices){
+                JPA.em().persist(office);
+            }
+
         });
         Logger.info("### Application has started and Offices inserted to DB");
     }
@@ -23,4 +39,29 @@ public class Global extends GlobalSettings {
         Logger.info("### Application shutdown...");
     }
 
+
+    public List<Office> readOfficesFromJsonFile(){
+        List<Office> offices = new ArrayList<>();
+
+
+
+        return offices;
+    }
+
+    public Office[] readJson() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+            URL url = new URL("https://service.bmf.gv.at/Finanzamtsliste.json");
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(),"UTF-8"));
+//            Office[] offices = mapper.readValue(url, Office[].class);
+            Office[] offices = mapper.readValue(in, Office[].class);
+            Logger.info("### size: "+ offices.length);
+            return offices;
+        }catch (Exception e){
+            Logger.error("### error: ",e);
+        }
+        return null;
+    }
 }
--- app/models/Office.java
+++ app/models/Office.java
@@ -1,5 +1,7 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,25 +10,53 @@ import javax.persistence.Id;
 @Entity
 public class Office {
 
+    public Office(){
+
+    }
+
+    public Office(String id, String name, String zipCode, String city, String street, String telephone,
+                  String openingHours, String imageUrl, String latitude, String longitude) {
+        this.id = id;
+        this.name = name;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.street = street;
+        this.telephone = telephone;
+        this.openingHours = openingHours;
+        this.imageUrl = imageUrl;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+//    @GeneratedValue(strategy=GenerationType.AUTO)
+    @JsonProperty("DisKz")
 	public String id;
 
+    @JsonProperty("DisNameLang")
     public String name;
 
+    @JsonProperty("DisPlz")
     public String zipCode;
 
+    @JsonProperty("DisOrt")
     public String city;
 
+    @JsonProperty("DisStrasse")
     public String street;
 
+    @JsonProperty("DisTel")
     public String telephone;
 
+    @JsonProperty("DisOeffnung")
     public String openingHours;
 
+    @JsonProperty("DisFotoUrl")
     public String imageUrl;
 
+    @JsonProperty("DisLatitude")
     public String latitude;
 
+    @JsonProperty("DisLongitude")
     public String longitude;
 }
